1fa4d12f9b13aea235e553f08f060935cfec73ef
Dharmendra Gopal Patel <dhpatel@redhat.com>
2020-10-21 17:34:11 +0530

    Support for command line args added

--- golang/build-manifest/gomanifest.go
+++ golang/build-manifest/gomanifest.go
@@ -9,16 +9,21 @@ import (
 	"strings"
 )
 
+/* Structure required for parsing `go list -deps -json ./...` json parsing */
+
+// GoPackages ... Package list structure from deps json
 type GoPackages struct {
 	Packages []GoPackage `json:"Packages"`
 }
 
+// GoModule ... Module structure from deps json
 type GoModule struct {
 	Path    string    `json:"Path"`
 	Version string    `json:"Version"`
 	Replace *GoModule `json:"Replace"`
 }
 
+// GoPackage ... Package structure from deps json
 type GoPackage struct {
 	Root       string   `json:"Root"`
 	ImportPath string   `json:"ImportPath"`
@@ -28,170 +33,52 @@ type GoPackage struct {
 	Deps       []string `json:"Deps"`
 }
 
-type DirectDeps struct {
-	Name        string
-	Version     string
-	Included    bool
-	Packages    []*DirectDeps
-	Transitives []*DirectDeps
+/* Structure for output the manifest */
+
+// Transitive ... Transitive details
+type Transitive struct {
+	Name     string   `json:"name"`
+	Version  string   `json:"version"`
+	Included bool     `json:"include"`
+	Packages []string `json:"packages"`
 }
 
-type TransDetails struct {
-	Name    string
-	Version string
-	Module  string
+// DirectPackage ... Direct package details
+type DirectPackage struct {
+	Name        string       `json:"name"`
+	Transitives []Transitive `json:"transitives"`
 }
 
-type TransDeps struct {
-	Name        string
-	Version     string
-	Module      string
-	Transitives []TransDetails
+// DirectDependency ... Direct dependency details
+type DirectDependency struct {
+	Name        string          `json:"name"`
+	Version     string          `json:"version"`
+	Included    bool            `json:"include"`
+	Packages    []DirectPackage `json:"packages"`
+	Transitives []Transitive    `json:"transitives"`
 }
 
-// Code root folder
-//const rootFolder = "/home/dhpatel/Documents/code/go-learn/sample"
-const rootFolder = "/home/dhpatel/Documents/code/go-learn/src/gitops-operator"
+// Manifest ... Final manifest file structure
+type Manifest struct {
+	Main         string             `json:"main"`
+	Dependencies []DirectDependency `json:"dependencies"`
+}
 
-var goExecutable string = ""
+// Source root folder, set via command line ARGS
+var sourceRootFolder = ""
+
+// Destination manifest file path, set via command line ARGS
+var manifestFilePath = ""
+
 var goPackages GoPackages
-var directDeps = make(map[string]DirectDeps)
-var directDepsJson string = ""
-var transDepsJson string = ""
+
+var mainModule string = ""
+var directDependencies = make(map[string]DirectDependency)
 
 func contains(s []string, searchterm string) bool {
 	sort.Strings(s)
 	i := sort.SearchStrings(s, searchterm)
 	return i < len(s) && s[i] == searchterm
-}
-
-func getGraphData() int {
-	// Get graph data
-	cmdGoModGraph := exec.Command(goExecutable, "mod", "graph")
-	cmdGoModGraph.Dir = rootFolder
-	if output, err := cmdGoModGraph.Output(); err != nil {
-		fmt.Printf("ERROR :: Command `%s mod graph` failed, resolve project build errors. %s\n", goExecutable, err)
-		return -1
-	} else {
-		for _, value := range strings.Split(string(output), "\n") {
-			if len(value) > 0 {
-				pc := strings.Split(value, " ")
-				if !strings.Contains(pc[0], "@") {
-					mv := strings.Split(pc[1], "@")
-					directDeps[mv[0]] = DirectDeps{mv[0], mv[1], false, make([]string, 0)}
-				}
-			}
-		}
-		fmt.Println(directDeps)
-	}
-
-	return 0
-}
-
-func getDepsData() int {
-	// Switch to code directory and get graph
-	cmdGoListDeps := exec.Command(goExecutable, "list", "-json", "-deps", "./...")
-	cmdGoListDeps.Dir = rootFolder
-	if output, err := cmdGoListDeps.Output(); err != nil {
-		fmt.Println("ERROR :: Command `go list -json -deps ./...` failed, resolve project build errors.", err)
-		return -1
-	} else {
-		goListDepsData := string(output)
-		goListDepsData = "{\"Packages\": [" + strings.ReplaceAll(goListDepsData, "}\n{", "},\n{") + "]}"
-
-		json.Unmarshal([]byte(goListDepsData), &goPackages)
-		fmt.Println(len(goPackages.Packages))
-	}
-	return 0
-}
-
-func buildDirectDeps(sourceImports []string) {
-	directDepsJson = ""
-	for mk, mod := range directDeps {
-		fmt.Println("Adding packages for direct deps", mod.Name, mod.Version)
-		for _, imp := range sourceImports {
-			if imp == mod.Name || strings.HasPrefix(imp, mod.Name+"/") {
-				fmt.Println(mod.Name, mod.Version, imp)
-				om := directDeps[mk]
-				if imp == mod.Name {
-					om.Included = true
-				} else {
-					om.Packages = append(directDeps[mk].Packages, imp)
-				}
-				directDeps[mk] = om
-			}
-		}
-		d, err := json.Marshal(directDeps[mk])
-		check(err)
-		if len(directDepsJson) == 0 {
-			directDepsJson = "\"direct_deps\": [" + string(d)
-		} else {
-			directDepsJson = directDepsJson + "," + string(d)
-		}
-	}
-	directDepsJson = directDepsJson + "],"
-	fmt.Println("Direct Deps :: ", directDeps)
-}
-
-func getTransDetails(modPath string, importPath string) []TransDetails {
-	var transDetails = make([]TransDetails, 0)
-	fmt.Println("Find Trans for importPath :: ", importPath)
-	for i := 0; i < len(goPackages.Packages); i++ {
-		if goPackages.Packages[i].ImportPath == importPath {
-			if goPackages.Packages[i].Standard == true {
-				fmt.Println("Skipping strandard import ::", importPath)
-				break
-			}
-
-			for _, dv := range goPackages.Packages[i].Deps {
-				for i := 0; i < len(goPackages.Packages); i++ {
-					if goPackages.Packages[i].Standard == false && goPackages.Packages[i].ImportPath == dv /*&& goPackages.Packages[i].Module.Path != modPath*/ {
-						transDetails = append(transDetails,
-							TransDetails{dv, goPackages.Packages[i].Module.Version, goPackages.Packages[i].Module.Path})
-					}
-				}
-			}
-		}
-	}
-	return transDetails
-}
-
-func buildTransitiveDeps() {
-	var transDeps = make(map[string]TransDeps)
-	transDepsJson = ""
-
-	for _, directDeps := range directDeps {
-		fmt.Println("Direct deps ::", directDeps.Name, directDeps.Version, directDeps.Included)
-		if directDeps.Included {
-			trans := getTransDetails(directDeps.Name, directDeps.Name)
-			if len(trans) > 0 {
-				transDeps[directDeps.Name] = TransDeps{directDeps.Name, directDeps.Version, directDeps.Name, trans}
-				d, err := json.Marshal(transDeps[directDeps.Name])
-				check(err)
-				if len(transDepsJson) == 0 {
-					transDepsJson = "\"transitive_deps\": [" + string(d)
-				} else {
-					transDepsJson = transDepsJson + "," + string(d)
-				}
-			}
-		}
-
-		for _, pckg := range directDeps.Packages {
-			trans := getTransDetails(directDeps.Name, pckg)
-			if len(trans) > 0 {
-				transDeps[pckg] = TransDeps{pckg, directDeps.Version, directDeps.Name, trans}
-				d, err := json.Marshal(transDeps[pckg])
-				check(err)
-				if len(transDepsJson) == 0 {
-					transDepsJson = "\"transitive_deps\": [" + string(d)
-				} else {
-					transDepsJson = transDepsJson + "," + string(d)
-				}
-			}
-		}
-	}
-	transDepsJson = transDepsJson + "]"
-	fmt.Println("Trans Deps :: ", transDeps)
 }
 
 func check(e error) {
@@ -200,39 +87,251 @@ func check(e error) {
 	}
 }
 
-func main() {
-	// Get 'go' executable path
-	goExe, _ := exec.LookPath("go")
-	if len(goExe) == 0 {
-		fmt.Println("No 'go' executable found on the system")
+func processGraphData() int {
+	// Get graph data
+	cmdGoModGraph := exec.Command("go", "mod", "graph")
+	cmdGoModGraph.Dir = sourceRootFolder
+	if output, err := cmdGoModGraph.Output(); err != nil {
+		fmt.Printf("ERROR :: Command `go mod graph` failed, resolve project build errors. %s\n", err)
+		return -1
 	} else {
-		goExecutable = goExe
-		fmt.Println("Found go executable ::", goExecutable)
-
-		if getGraphData() == 0 {
-			if getDepsData() == 0 {
-				// Get direct imports and deps from current source.
-				var sourceImports []string
-				for i := 0; i < len(goPackages.Packages); i++ {
-					if goPackages.Packages[i].Standard == false && !strings.Contains(goPackages.Packages[i].Root, "@") {
-						fmt.Println("ImportPath: " + goPackages.Packages[i].ImportPath + " " + goPackages.Packages[i].Root)
-						for _, imp := range goPackages.Packages[i].Imports {
-							if !contains(sourceImports, imp) {
-								sourceImports = append(sourceImports, imp)
-							}
-						}
-					}
+		for _, value := range strings.Split(string(output), "\n") {
+			if len(value) > 0 {
+				// Extract direct dependency from go.mod
+				pc := strings.Split(value, " ")
+				if !strings.Contains(pc[0], "@") {
+					mainModule = pc[0]
+					mv := strings.Split(pc[1], "@")
+					directDependencies[mv[0]] = DirectDependency{mv[0], mv[1], false, make([]DirectPackage, 0), make([]Transitive, 0)}
 				}
-				buildDirectDeps(sourceImports)
-				buildTransitiveDeps()
+			}
+		}
+		fmt.Println("Direct dependencies: ", len(directDependencies))
+	}
 
-				f, err := os.Create(rootFolder + "/target/clientManifest.json")
-				check(err)
-				_, err = f.WriteString("{" + directDepsJson + transDepsJson + "}")
-				check(err)
-				f.Sync()
+	return 0
+}
 
-				defer f.Close()
+func processDepsData() int {
+	// Switch to code directory and get graph
+	cmdGoListDeps := exec.Command("go", "list", "-json", "-deps", "./...")
+	cmdGoListDeps.Dir = sourceRootFolder
+	if output, err := cmdGoListDeps.Output(); err != nil {
+		fmt.Println("ERROR :: Command `go list -json -deps ./...` failed, resolve project build errors.", err)
+		return -1
+	} else {
+		goListDepsData := string(output)
+		goListDepsData = "{\"Packages\": [" + strings.ReplaceAll(goListDepsData, "}\n{", "},\n{") + "]}"
+
+		json.Unmarshal([]byte(goListDepsData), &goPackages)
+		fmt.Println("Total packages in deps: ", len(goPackages.Packages))
+	}
+	return 0
+}
+
+func buildDirectDependencies() {
+	// Get direct imports from current source.
+	var sourceImports []string
+	for i := 0; i < len(goPackages.Packages); i++ {
+		// Exclude standard packages and include only packages with project ROOT
+		if goPackages.Packages[i].Standard == false && !strings.Contains(goPackages.Packages[i].Root, "@") {
+			for _, imp := range goPackages.Packages[i].Imports {
+				if !contains(sourceImports, imp) {
+					sourceImports = append(sourceImports, imp)
+				}
+			}
+		}
+	}
+	fmt.Println("Total imports: ", len(sourceImports))
+
+	var importsAdded = 0
+	for mk, mod := range directDependencies {
+		for _, imp := range sourceImports {
+			if imp == mod.Name || strings.HasPrefix(imp, mod.Name+"/") {
+				om := directDependencies[mk]
+				if imp == mod.Name {
+					om.Included = true
+				} else {
+					om.Packages = append(directDependencies[mk].Packages, DirectPackage{imp, make([]Transitive, 0)})
+				}
+				directDependencies[mk] = om
+				importsAdded++
+			}
+		}
+	}
+	fmt.Println("Total imports as direct dependencies: ", importsAdded)
+}
+
+func findAndAddTransitive(importPath string, transitivies []Transitive) []Transitive {
+	for i := 0; i < len(goPackages.Packages); i++ {
+		if goPackages.Packages[i].Standard == false && goPackages.Packages[i].ImportPath == importPath {
+			var foundModule = false
+			for t := 0; t < len(transitivies); t++ {
+				if transitivies[t].Name == goPackages.Packages[i].Module.Path {
+					if importPath == goPackages.Packages[i].Module.Path {
+						transitivies[t].Included = true
+					} else {
+						transitivies[t].Packages = append(transitivies[t].Packages, importPath)
+					}
+					foundModule = true
+				}
+			}
+
+			if !foundModule {
+				var newTrans = Transitive{
+					goPackages.Packages[i].Module.Path,
+					goPackages.Packages[i].Module.Version,
+					importPath == goPackages.Packages[i].Module.Path,
+					make([]string, 0)}
+				if importPath != goPackages.Packages[i].Module.Path {
+					newTrans.Packages = append(newTrans.Packages, importPath)
+				}
+				transitivies = append(transitivies, newTrans)
+			}
+		}
+	}
+	return transitivies
+}
+
+func getTransitiveDetails(modPath string, importPath string) []Transitive {
+	var transitivies = make([]Transitive, 0)
+	for i := 0; i < len(goPackages.Packages); i++ {
+		if goPackages.Packages[i].ImportPath == importPath {
+			if goPackages.Packages[i].Standard == true {
+				fmt.Println("Skipping strandard import ::", importPath)
+				break
+			}
+
+			for _, dv := range goPackages.Packages[i].Deps {
+				transitivies = findAndAddTransitive(dv, transitivies)
+			}
+		}
+	}
+	return transitivies
+}
+
+func buildTransitiveDeps() {
+	for k, ddeps := range directDependencies {
+		var dm = directDependencies[k]
+		if ddeps.Included {
+			dm.Transitives = getTransitiveDetails(ddeps.Name, ddeps.Name)
+		}
+		dm.Packages = make([]DirectPackage, 0)
+
+		for _, pckg := range ddeps.Packages {
+			pckg.Transitives = getTransitiveDetails(ddeps.Name, pckg.Name)
+			dm.Packages = append(dm.Packages, pckg)
+		}
+		directDependencies[k] = dm
+	}
+	fmt.Println("Added transitive dependencies")
+}
+
+func buildManifest() {
+	var manifest Manifest = Manifest{mainModule, make([]DirectDependency, 0)}
+	for mk := range directDependencies {
+		manifest.Dependencies = append(manifest.Dependencies, directDependencies[mk])
+	}
+
+	d, err := json.Marshal(manifest)
+	check(err)
+	var directDependenciesJSON string = string(d)
+
+	f, err := os.Create(manifestFilePath)
+	check(err)
+	_, err = f.WriteString(string(directDependenciesJSON))
+	check(err)
+	f.Sync()
+
+	defer f.Close()
+}
+
+func flatenTransitives(transtivies []Transitive) string {
+	var returnString = ""
+	var firstEntry = true
+	for _, t := range transtivies {
+		if t.Included {
+			if !firstEntry {
+				returnString = returnString + ","
+			} else {
+				firstEntry = false
+			}
+			//returnString = returnString + "{ \"" + t.Name + "\": {\"name\": \"" + t.Name + "\", \"version\": \"" + t.Version + "\"} }"
+			returnString = returnString + "{\"name\": \"" + t.Name + "\", \"version\": \"" + t.Version + "\"}"
+		}
+
+		for _, p := range t.Packages {
+			if !firstEntry {
+				returnString = returnString + ","
+			} else {
+				firstEntry = false
+			}
+			//returnString = returnString + "{ \"" + p + "@" + t.Name + "\": {\"name\": \"" + p + "@" + t.Name + "\", \"version\": \"" + t.Version + "\"} }"
+			returnString = returnString + "{\"name\": \"" + p + "@" + t.Name + "\", \"version\": \"" + t.Version + "\"}"
+		}
+	}
+
+	return "[" + returnString + "]"
+}
+
+func buildManifest2() {
+	var directDependenciesJSON string = "{\"main\": \"" + mainModule + "\", \"packages\": ["
+
+	var firstEntry = true
+	for _, mod := range directDependencies {
+		if mod.Included {
+			if !firstEntry {
+				directDependenciesJSON = directDependenciesJSON + ","
+			} else {
+				firstEntry = false
+			}
+			directDependenciesJSON = directDependenciesJSON + "{\"name\": \"" + mod.Name + "\", \"version\": \"" + mod.Version + "\","
+			directDependenciesJSON = directDependenciesJSON + "\"dependencies\": " + flatenTransitives(mod.Transitives)
+			directDependenciesJSON = directDependenciesJSON + "}"
+		}
+
+		for _, pckg := range mod.Packages {
+			if !firstEntry {
+				directDependenciesJSON = directDependenciesJSON + ","
+			} else {
+				firstEntry = false
+			}
+			directDependenciesJSON = directDependenciesJSON + "{\"name\": \"" + pckg.Name + "@" + mod.Name + "\", \"version\": \"" + mod.Version + "\","
+			directDependenciesJSON = directDependenciesJSON + "\"dependencies\": " + flatenTransitives(pckg.Transitives)
+			directDependenciesJSON = directDependenciesJSON + "}"
+		}
+	}
+	directDependenciesJSON = directDependenciesJSON + "]}"
+
+	f, err := os.Create(manifestFilePath)
+	check(err)
+	_, err = f.WriteString(string(directDependenciesJSON))
+	check(err)
+	f.Sync()
+
+	defer f.Close()
+}
+
+func main() {
+	if (len(os.Args) != 3) {
+		fmt.Println("Error :: Invalid arguments for the command.")
+		fmt.Println("Usage :: gomanifest <Absolute source root folder path containing go.mod> <Output file path>.json")
+		fmt.Println("")
+		fmt.Println("Example :: gomanifest /home/user/goproject/root/folder /home/user/gomanifest.json")
+	} else {
+		_, err := os.Stat(os.Args[1])
+		if (err != nil) {
+			fmt.Println("Invalid source folder path ::", os.Args[1])
+		} else {
+			fmt.Println("Building manifest file for ::", os.Args[1])
+			sourceRootFolder = os.Args[1]
+			manifestFilePath = os.Args[2]
+			if processGraphData() == 0 {
+				if processDepsData() == 0 {
+					buildDirectDependencies()
+					buildTransitiveDeps()
+					buildManifest2()
+				}
 			}
 		}
 	}
